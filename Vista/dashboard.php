76ad096c37922192edeeaff497c6584f04eb21b5
mpquiones0 <126877653+mpquiones0@users.noreply.github.com>
2023-06-03 19:12:55 -0500

    Update

--- Vista/dashboard.php
+++ Vista/dashboard.php
@@ -30,7 +30,7 @@
                 </a></li>
 
                 <li><a href="">
-                    <i class="uil uil-bookmark"></i>
+                    <i class="uil uil-shopping-bag"></i>
                     <span class="link-name">Productos</span>
                 </a></li>
 
@@ -73,22 +73,75 @@
         <div class="top">
             <i class="uil uil-bars"></i>
 
-            <div class="buscar">
+            <div class="search-box">
                 <input type="text" placeholder="¿Qué buscas?">
                 <i class="uil uil-search"></i>
-
             </div>
+
+            <img src="/imagenes/avatar.PNG" alt="">
+
         </div>
 
         <div class="dash-content">
             <div class="overview">
-                <div class="text">
-                    
-
+                <div class="title">
+                    <i class="uil uil-bookmark"></i>
+                    <span class="text">Dashboard</span>
                 </div>
 
-            </div>
+                <div class="boxes">
+                    <div class="box">
+                        <div class="box1">
+                            <span class="text">Usuarios</span>
+                            <i class="uil uil-users-alt"></i>
+                            <span class="number">20</span>
+                        </div>
 
+                        <div class="box2">
+                            <span class="text">Productos</span>
+                            <i class="uil uil-shopping-bag"></i>
+                            <span class="number">500</span>
+                        </div>
+
+                        <div class="box3">
+                            <span class="text">Ventas</span>
+                            <i class="uil uil-bill"></i>
+                            <span class="number">80</span>
+                        </div>
+
+                        <div class="box4">
+                            <span class="text">Pedidos</span>
+                            <i class="uil uil-shopping-cart"></i>
+                            <span class="number">60</span>
+                        </div>
+
+                        <div class="box5">
+                            <span class="text">Domicilios</span>
+                            <i class="uil uil-truck"></i>
+                            <span class="number">60</span>
+                        </div>
+
+                        <div class="box6">
+                            <span class="text">Compras</span>
+                            <i class="uil uil-store"></i>
+                            <span class="number" >40</span>
+                        </div>
+
+                        <div class="box7">
+                            <span class="text">Comentarios</span>
+                            <i class="uil uil-comments"></i>
+                            <span class="number">30</span>
+                        </div>
+
+                        <div class="box8">
+                            <span class="text">Calificaciones</span>
+                            <i class="uil uil-star"></i>
+                            <span class="number">30</span>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
 
 
